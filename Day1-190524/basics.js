632377c348115251e1bec323532cabf7cf7d843c
Vasanthkumar <vasizebron10@gmail.com>
2024-05-20 21:04:03 +0530

    day2 - js lifecycle, structure, prototypes, objs

---
+++ Day1-190524/basics.js
@@ -0,0 +1,4 @@
+// Javascript - Scripting language
+// A single threaded synchronous programming language
+// Javascript will never ever wait for anyone
+// Event Queue -> Call Stack + WebAPI + Macrotask Queue + Microtask Queue
